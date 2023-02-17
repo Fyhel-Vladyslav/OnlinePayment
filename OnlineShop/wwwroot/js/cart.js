73892c6537b2b6f14cee29d196a4feb79a328259
Фугель Владислав <102908868+Fyhel-Vladyslav@users.noreply.github.com>
2023-02-17 23:42:29 +0200

    Add cart

---
+++ OnlineShop/wwwroot/js/cart.js
@@ -0,0 +1,20 @@
+﻿function plus() {
+	let rel = document.getElementById("amount");
+	if (rel.value < 999)
+		rel.value = Number(rel.value) + 1;
+	else
+		alert("Максимальне число одиниць 999");
+}
+
+function minus() {
+	let rel = document.getElementById("amount");
+	if (rel.value > 0)
+		if (rel.value > 1)
+			rel.value = Number(rel.value) - 1;
+		else
+			deleteItem();
+}
+
+function deleteItem() {
+
+}
